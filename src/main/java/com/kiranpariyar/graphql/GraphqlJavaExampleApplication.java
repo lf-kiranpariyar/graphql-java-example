a6e92f63d5a1d03943005b4bd8e6fb668cd19832
Kiran Pariyar <kirannpariyar@gmail.com>
2020-01-18 20:31:17 +0545

    Create graphql java example

---
+++ src/main/java/com/kiranpariyar/graphql/GraphqlJavaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kiranpariyar.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJavaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiranpariyar/graphql/graphqljavaexample/GraphqlJavaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiranpariyar.graphql.graphqljavaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJavaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
